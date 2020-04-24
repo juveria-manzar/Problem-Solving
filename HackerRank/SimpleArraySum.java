585499d6803510b32fdc27ba3783217985daa65c
Juveria Manzar <juveriadot@gmail.com>
2020-04-24 15:40:41 +0530

    warmup

--- HackerRank/SimpleArraySum.java
+++ HackerRank/SimpleArraySum.java
@@ -4,7 +4,7 @@ import java.text.*;
 import java.util.*;
 import java.util.regex.*;
 
-public class Solution {
+public class SimpleArraySum {
 
     /*
      * Complete the simpleArraySum function below.
